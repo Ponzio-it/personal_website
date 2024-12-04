18cd91063c0e7c57fe74ec88b15353a81792daf7
ettore <ponzioettore@outlook.com>
2024-12-04 11:39:58 +0100

    fixing eliminating  base dir templates

--- mypersonalsite/settings/base.py
+++ mypersonalsite/settings/base.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'mypersonalsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'portfolio/templates/portfolio'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
