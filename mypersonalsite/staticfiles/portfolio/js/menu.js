2ba65328e7fb8ebcb198692a38bc424bb81cb968
ettore <ponzioettore@outlook.com>
2024-12-05 09:55:02 +0100

    removing staticfiles dir

--- mypersonalsite/staticfiles/portfolio/js/menu.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('mobile-menu').addEventListener('click', function () {
-    const navLinks = document.querySelector('.nav-links');
-    navLinks.classList.toggle('active');
-});
